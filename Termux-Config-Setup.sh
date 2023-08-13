6186b199dd626b83454fc3d159dd71446ff25539
Mercenario <83768808+SnowPerfectDev@users.noreply.github.com>
2023-08-13 14:00:02 -0300

    Update Termux-Config-Setup.sh

--- Termux-Config-Setup.sh
+++ Termux-Config-Setup.sh
@@ -1,49 +1,43 @@
 #!/bin/bash
 
-# Script para configurar as opções do Termux
-
 # Cores ANSI
 vermelho="\033[1;31m"
 verde="\033[1;32m"
 reset="\033[0m"
 
-# Função para remover arquivos dentro da pasta ../usr/etc/
-remove_files() {
-    echo -e "[${verde}+${reset}] Removendo arquivos..."
-
-    cd ~/../usr/etc/ || exit
-
-    # Verificar se os arquivos existem antes de removê-los
-    if [ -e motd.sh.dpkg-old ]; then
-        rm -f motd.sh.dpkg-old
-        echo -e "[${verde}+${reset}] Arquivo motd.sh.dpkg-old removido."
-    fi
-
-    if [ -e motd-playstore ]; then
-        rm -f motd-playstore
-        echo -e "[${verde}+${reset}] Arquivo motd-playstore removido."
-    fi
-
-    if [ -e motd.sh ]; then
-        rm -f motd.sh
-        echo -e "[${verde}+${reset}] Arquivo motd.sh removido."
-    fi
-
-    if [ -e motd ]; then
-        rm -f motd
-        echo -e "[${verde}+${reset}] Arquivo motd removido."
-    fi
-
-    echo -e "[${verde}+${reset}] Arquivos removidos com sucesso."
-    cd - >/dev/null || exit
+# Função para exibir mensagens de sucesso em verde
+exibir_sucesso() {
+    mensagem="$1"
+    echo -e "[${verde}+${reset}] ${mensagem}"
 }
 
-# Função para fazer backup de um arquivo
-fazer_backup() {
-    if [ -e "$1" ]; then
-        cp "$1" "$1.bkp"
-        echo -e "[${vermelho}+${reset}] Backup de $1 criado como $1.bkp"
-    fi
+# Função para exibir mensagens de erro em vermelho
+exibir_erro() {
+    mensagem="$1"
+    echo -e "[${vermelho}!${reset}] ${mensagem}"
+}
+
+# Função para remover arquivos dentro da pasta ../usr/etc/
+remove_files() {
+    exibir_sucesso "Removendo arquivos..."
+
+    cd ~/../usr/etc/ || {
+        exibir_erro "Não foi possível acessar o diretório."
+        exit 1
+    }
+
+    # Arquivos a serem removidos
+    arquivos=("motd" "motd.sh" "motd-playstore")
+    
+    for arquivo in "${arquivos[@]}"; do
+        if [ -e "$arquivo" ]; then
+            rm -f "$arquivo"
+            exibir_sucesso "Arquivo $arquivo removido."
+        fi
+    done
+
+    exibir_sucesso "Arquivos removidos com sucesso."
+    cd - >/dev/null || exit
 }
 
 # Função para baixar e aplicar uma configuração
@@ -52,43 +46,43 @@ baixar_e_aplicar() {
     ARQUIVO="$2"
     PASTA_DESTINO="$3"
     
-    fazer_backup "$PASTA_DESTINO/$ARQUIVO"
+    # Fazer backup se o arquivo existir
+    if [ -e "$PASTA_DESTINO/$ARQUIVO" ]; then
+        cp "$PASTA_DESTINO/$ARQUIVO" "$PASTA_DESTINO/$ARQUIVO.bkp"
+        exibir_sucesso "Backup de $ARQUIVO criado como $ARQUIVO.bkp"
+    fi
     
     curl -sLo "$PASTA_DESTINO/$ARQUIVO" "$URL"
-    echo -e "[${verde}+${reset}] Configuração $ARQUIVO aplicada"
+    exibir_sucesso "Configuração $ARQUIVO aplicada"
 }
 
-echo -e "[${verde}+${reset}] Iniciando configurações do Termux..."
+exibir_sucesso "Iniciando configurações do Termux..."
+
+# URLs centralizadas
+declare -A urls=(
+    ["bash.bashrc"]="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/prompt-settings/bash-configs/bash.bashrc"
+    ["termux.properties"]="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/prompt-settings/termux-configs/termux.properties"
+    [".nanorc"]="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/shell-config/config-files/.nanorc"
+    ["colors.properties"]="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/prompt-settings/color-schemes/colors.properties"
+    ["font.ttf"]="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/shell-config/config-files/font.ttf"
+    [".bashrc"]="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/prompt-settings/bash-configs/.bashrc"
+)
 
 # Chamar a função para remover os arquivos
 remove_files
 
-# URL para bash.bashrc (URL original)
-URL_BASH_BASHRC="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/prompt-settings/bash-configs/bash.bashrc"
-# URL para termux.properties
-URL_TERMUX_PROPERTIES="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/prompt-settings/termux-configs/termux.properties"
-# URL para .nanorc
-URL_NANORC="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/shell-config/config-files/.nanorc"
-# URL para colors.properties
-URL_COLORS_PROPERTIES="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/prompt-settings/color-schemes/colors.properties"
-# URL para font.ttf
-URL_FONT="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/shell-config/config-files/font.ttf"
-# URL para .bashrc (URL principal do repositório)
-URL_BASHRC="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/prompt-settings/bash-configs/.bashrc"
+# Baixar e aplicar as configurações
+for arq in "${!urls[@]}"; do
+    if [ "$arq" = ".nanorc" ]; then
+        destino="$HOME"  # Copiar o .nanorc para a pasta home
+    elif [ "$arq" = ".bashrc" ]; then
+        destino="$HOME"  # Copiar o .bashrc para a pasta home
+    elif [ "$arq" = "bash.bashrc" ]; then
+        destino="$HOME/../usr/etc"  # Copiar o bash.bashrc para a pasta ~/../usr/etc
+    else
+        destino="$HOME/.termux"  # Demais arquivos vão para a pasta .termux
+    fi
+    baixar_e_aplicar "${urls[$arq]}" "$arq" "$destino"
+done
 
-# Baixa e aplica a configuração do arquivo bash.bashrc
-baixar_e_aplicar "$URL_BASH_BASHRC" "bash.bashrc" "/data/data/com.termux/files/usr/etc"
-# Baixa e aplica a configuração do arquivo termux.properties
-mkdir -p "$HOME/.termux"
-baixar_e_aplicar "$URL_TERMUX_PROPERTIES" "termux.properties" "$HOME/.termux"
-# Baixa e aplica a configuração do arquivo .nanorc
-baixar_e_aplicar "$URL_NANORC" ".nanorc" "$HOME"
-# Baixa e aplica a configuração do arquivo colors.properties
-mkdir -p "$HOME/.termux"
-baixar_e_aplicar "$URL_COLORS_PROPERTIES" "colors.properties" "$HOME/.termux"
-# Baixa o arquivo font.ttf
-mkdir -p "$HOME/.termux"
-curl -sLo "$HOME/.termux/font.ttf" "$URL_FONT"
-echo -e "[${verde}+${reset}] Arquivo font.ttf baixado para $HOME/.termux/font.ttf"
-# Baixa e aplica a configuração do arquivo .bashrc
-baixar_e_aplicar "$URL_BASHRC" ".bashrc" "$HOME"
+exibir_sucesso "Configurações concluídas."
