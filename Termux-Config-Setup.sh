5eb141796b085413a88da2726c95255d40f0cf39
Mercenario <83768808+SnowPerfectDev@users.noreply.github.com>
2023-08-13 11:38:44 -0300

    Update Termux-Config-Setup.sh

--- Termux-Config-Setup.sh
+++ Termux-Config-Setup.sh
@@ -9,40 +9,40 @@ reset="\033[0m"
 
 # Função para remover arquivos dentro da pasta ../usr/etc/
 remove_files() {
-    echo "Removendo arquivos..."
+    echo -e "[${verde}+${reset}] Removendo arquivos..."
 
-    cd ~/../usr/etc/
+    cd ~/../usr/etc/ || exit
 
     # Verificar se os arquivos existem antes de removê-los
     if [ -e motd.sh.dpkg-old ]; then
         rm -f motd.sh.dpkg-old
-        echo "Arquivo motd.sh.dpkg-old removido."
+        echo -e "[${verde}+${reset}] Arquivo motd.sh.dpkg-old removido."
     fi
 
     if [ -e motd-playstore ]; then
         rm -f motd-playstore
-        echo "Arquivo motd-playstore removido."
+        echo -e "[${verde}+${reset}] Arquivo motd-playstore removido."
     fi
 
     if [ -e motd.sh ]; then
         rm -f motd.sh
-        echo "Arquivo motd.sh removido."
+        echo -e "[${verde}+${reset}] Arquivo motd.sh removido."
     fi
 
     if [ -e motd ]; then
         rm -f motd
-        echo "Arquivo motd removido."
+        echo -e "[${verde}+${reset}] Arquivo motd removido."
     fi
 
-    echo "Arquivos removidos com sucesso."
-    cd - >/dev/null
+    echo -e "[${verde}+${reset}] Arquivos removidos com sucesso."
+    cd - >/dev/null || exit
 }
 
 # Função para fazer backup de um arquivo
 fazer_backup() {
     if [ -e "$1" ]; then
         cp "$1" "$1.bkp"
-        echo -e "${vermelho}Backup de $1 criado como $1.bkp${reset}"
+        echo -e "[${vermelho}+${reset}] Backup de $1 criado como $1.bkp"
     fi
 }
 
@@ -55,10 +55,10 @@ baixar_e_aplicar() {
     fazer_backup "$PASTA_DESTINO/$ARQUIVO"
     
     curl -sLo "$PASTA_DESTINO/$ARQUIVO" "$URL"
-    echo -e "${verde}Configuração $ARQUIVO aplicada${reset}"
+    echo -e "[${verde}+${reset}] Configuração $ARQUIVO aplicada"
 }
 
-echo -e "${verde}Iniciando configurações do Termux...${reset}"
+echo -e "[${verde}+${reset}] Iniciando configurações do Termux..."
 
 # Chamar a função para remover os arquivos
 remove_files
@@ -72,7 +72,7 @@ URL_NANORC="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup
 # URL para colors.properties
 URL_COLORS_PROPERTIES="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/prompt-settings/color-schemes/colors.properties"
 # URL para font.ttf
-URL_FONT_TTF="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/shell-config/config-files/font.ttf"
+URL_FONT="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/shell-config/config-files/font.ttf"
 # URL para .bashrc (URL principal do repositório)
 URL_BASHRC="https://raw.githubusercontent.com/SnowPerfectDev/Termux-Config-Setup/main/prompt-settings/bash-configs/.bashrc"
 
@@ -86,10 +86,8 @@ baixar_e_aplicar "$URL_NANORC" ".nanorc" "$HOME"
 # Baixa e aplica a configuração do arquivo colors.properties
 mkdir -p "$HOME/.termux"
 baixar_e_aplicar "$URL_COLORS_PROPERTIES" "colors.properties" "$HOME/.termux"
-# Baixa e aplica o arquivo font.ttf
+# Baixa o arquivo font.ttf
 mkdir -p "$HOME/.termux"
-baixar_e_aplicar "$URL_FONT_TTF" "font.ttf" "$HOME/.termux"
-# Baixa e aplica o arquivo .bashrc
-baixar_e_aplicar "$URL_BASHRC" ".bashrc" "$HOME"
-
-echo -e "${verde}Configurações concluídas!${reset}"
+curl -sLo "$HOME/.termux/font.ttf" "$URL_FONT"
+echo -e "[${verde}+${reset}] Arquivo font.ttf baixado para $HOME/.termux/font.ttf"
+#
